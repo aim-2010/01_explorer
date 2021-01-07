38a0ae88234ff68bb59dc6f4273e6684b84978d3
aim-2010 <76846697+aim-2010@users.noreply.github.com>
2021-01-07 16:36:36 +0100

    Aktualisiere pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,33 +1,9 @@
 input.onButtonPressed(Button.A, function () {
-    GO = 0
-    basic.showLeds(`
-        . # # # .
-        . . . # .
-        . # # # .
-        . . . # .
-        . # # # .
-        `)
-    basic.showLeds(`
-        . # # # .
-        . . . # .
-        . # # # .
-        . # . . .
-        . # # # .
-        `)
-    basic.showLeds(`
-        . . # . .
-        . # # . .
-        . . # . .
-        . . # . .
-        . # # # .
-        `)
-    basic.showNumber(Proxi.Lese_LBlock())
-    basic.showNumber(Proxi.Lese_RBlock())
+    GO = 1
     music.startMelody(music.builtInMelody(Melodies.PowerUp), MelodyOptions.Once)
-    basic.pause(100)
 })
 input.onButtonPressed(Button.B, function () {
-    GO = 1
+    GO = 0
     basic.showLeds(`
         # . . . #
         . # . # .
@@ -43,38 +19,21 @@ let GO = 0
 GO = 0
 basic.forever(function () {
     if (GO > 0) {
-        Proxi.vorwärts()
-        if (Proxi.LBlock(25)) {
-            Proxi.stehenbleiben()
-            Proxi.linksdrehung()
-            images.createImage(`
-                . . # . .
-                . . . # .
-                # # # # #
-                . . . # .
-                . . # . .
-                `).scrollImage(1, 100)
-            Proxi.drehungsstopp()
-        } else if (Proxi.RBlock(25)) {
-            Proxi.stehenbleiben()
-            Proxi.rechtsdrehung()
-            images.createImage(`
-                . . # . .
-                . # . . .
-                # # # # #
-                . # . . .
-                . . # . .
-                `).scrollImage(1, 100)
-            Proxi.stehenbleiben()
-        }
         basic.showLeds(`
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
-            . . . . .
+            . # . . .
+            . # . . .
+            . # . . .
+            . # . . .
+            . # # # .
             `)
+        basic.showNumber(Proxi.Lese_LBlock())
+        basic.showLeds(`
+            . # # . .
+            . # . # .
+            . # # . .
+            . # . # .
+            . # . # .
+            `)
+        basic.showNumber(Proxi.Lese_RBlock())
     }
-    Proxi.drehungsstopp()
-    Proxi.stehenbleiben()
 })
